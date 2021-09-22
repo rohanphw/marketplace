01a16763010c336aec8df4527469a3859ff4f190
Rohan pahwa <rohanphww@gmail.com>
2021-09-22 11:15:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+import { ethers } from "ethers"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import Web3Modal from  "web3modal"
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <h1>Home</h1>
  
     </div>
   )
